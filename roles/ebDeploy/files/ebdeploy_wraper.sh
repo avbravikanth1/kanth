797f7e565def052dda0a7baadd15bd007fc0b285
ravikanth <ravikanth.ado@gmail.com>
2022-07-27 14:45:15 +0530

    added

--- roles/ebDeploy/files/ebdeploy_wraper.sh
+++ roles/ebDeploy/files/ebdeploy_wraper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # cd /opt/test_app && eb init --region ap-southeast-1 --platform Node.js-16 eb_test && eb deploy
-cd /opt/test_app && eb init --region ap-southeast-1 --platform arn:aws:elasticbeanstalk:ap-southeast-1:236894363456:platform/Node.js/16 --application arn:aws:elasticbeanstalk:ap-southeast-1:236894363456:application/eb_test  --environment arn:aws:elasticbeanstalk:ap-southeast-1:236894363456:environment/eb_test/Ebtest-env
+cd /opt/test_app && eb init --region ap-southeast-1 --platform arn:aws:elasticbeanstalk:ap-southeast-1:236894363456:platform/Node.js/16 --application-name arn:aws:elasticbeanstalk:ap-southeast-1:236894363456:application/eb_test  --environment-name arn:aws:elasticbeanstalk:ap-southeast-1:236894363456:environment/eb_test/Ebtest-env
 
 #eb init --profile eb-cli --region us-east-1 --platform "arn:aws:elasticbeanstalk:us-east-1::platform/Tomcat 8.5 with Java 8 running on 64bit Amazon Linux/3.3.0" ${{ parameters.ENVIRONMENT }} -v
 
