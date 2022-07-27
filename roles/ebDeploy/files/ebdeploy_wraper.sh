b760cc82f6c1b7c9989e0f9c76506581fe80720c
ravikanth <ravikanth.ado@gmail.com>
2022-07-27 18:20:52 +0530

    added

--- roles/ebDeploy/files/ebdeploy_wraper.sh
+++ roles/ebDeploy/files/ebdeploy_wraper.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-#cd /opt/test_app | eb init -i --region ap-southeast-1 --platform "arn:aws:elasticbeanstalk:ap-southeast-1::platform/Node.js 16 running on 64bit Amazon Linux 2/5.5.4" eb_test | eb use Ebtest-env | eb deploy
+cd /opt/test_app | eb init -i --region ap-southeast-1 --platform "arn:aws:elasticbeanstalk:ap-southeast-1::platform/Node.js 16 running on 64bit Amazon Linux 2/5.5.4" eb_test | eb use Ebtest-env | eb deploy
 # cd /opt/test_app | eb deploy Ebtest-env  ----> "app-version is creating"
-  cd /opt/test_app |eb deploy --label version_label ebtest-env
 # cd /opt/test_app | eb init --region ap-southeast-1 eb_test #--platform Node.js 16 eb_test | eb deploy Ebtest-env
 # cd /opt/test_app | printf '\n\n\n\n\n' | eb init eb_test --region ap-southeast-1 | eb deploy --verbose -l Ebtest-env
 # cd /opt/test_app && eb init --region ap-southeast-1 --platform arn:aws:elasticbeanstalk:ap-southeast-1:236894363456:platform/Node.js/16 --application-name arn:aws:elasticbeanstalk:ap-southeast-1:236894363456:application/eb_test  --environment-name arn:aws:elasticbeanstalk:ap-southeast-1:236894363456:environment/eb_test/Ebtest-env
