2f11fc3194429b914a41d0d7bc5c5d1642c66a25
ravikanth <ravikanth.ado@gmail.com>
2022-07-27 13:19:44 +0530

    added

--- roles/ebDeploy/files/ebdeploy_wraper.sh
+++ roles/ebDeploy/files/ebdeploy_wraper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /opt/test_app && eb init --region ap-southeast-1 --platform Node.js eb_test && eb deploy
+cd /opt/test_app && eb init --region ap-southeast-1 --platform Node.js 16 eb_test && eb deploy
 
 
